0071c786990aa2c38c113ec35433aedc013aee57
JallouliMohamed <medahmed.jallouli@gmail.com>
2019-09-08 21:55:25 +0200

    first commit

---
+++ src/UsersBundle/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace UsersBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /*public function indexAction()
+    {
+        return $this->render('UsersBundle:Default:index.html.twig');
+    }*/
+
+    public function indexAction()
+    {
+        return $this->render('::base.html.twig');
+    }
+
+
+}
